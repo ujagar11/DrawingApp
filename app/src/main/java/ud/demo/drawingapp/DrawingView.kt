10d2920193d0c09970b999567d6bcff42c4662e1
ujagar11 <ujagar.xi@gmail.com>
2024-01-11 00:32:28 +0530

    "brush size and drawpaths saving"

--- app/src/main/java/ud/demo/drawingapp/DrawingView.kt
+++ app/src/main/java/ud/demo/drawingapp/DrawingView.kt
@@ -4,19 +4,22 @@ import android.content.Context
 import android.graphics.*
 
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context:Context,attrs: AttributeSet): View(context,attrs) {
-
+    //it represents the current path being drawn
     private var mDrawPath:CustomPath?=null
     private  var mCanvasBitmap:Bitmap?=null
     private var mDrawPaint: Paint?=null
+    //used to handle canvas when drawing the bitmap
     private var mCanvasPaint:Paint?=null
     private var mBrushSize: Float=0.toFloat()
     private var color =Color.BLACK
     private var canvas:Canvas?=null
 
+    private val mPaths =ArrayList<CustomPath>()
     init {
         setUpDrawing()
     }
@@ -28,23 +31,29 @@ class DrawingView(context:Context,attrs: AttributeSet): View(context,attrs) {
         mDrawPaint!!.style=Paint.Style.STROKE
         mDrawPaint!!.strokeJoin=Paint.Join.ROUND
         mDrawPaint!!.strokeCap=Paint.Cap.ROUND
-
+        //DITHER_FLAG is used to increase the rendering quality of paint or drawing
         mCanvasPaint=Paint(Paint.DITHER_FLAG)
-        mBrushSize=20.toFloat()
+      //  mBrushSize=0.toFloat() no need set we are set size from main by brushSize func()
 
 
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        // we have created a Canvas object which is a 2d surface for drawing and content on that surface will be hold by bitmap
         mCanvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
-
         canvas= Canvas(mCanvasBitmap!!)
     }
-    //Canvas? to Canvas has been done v
+    //Canvas? to Canvas has been done
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+        //showing saved paths
+        for (path in mPaths){
+            mDrawPaint!!.strokeWidth=path!!.brushThickness
+            mDrawPaint!!.color=path!!.color
+            canvas.drawPath(path!!,mDrawPaint!!)
+        }
        if(!mDrawPath!!.isEmpty) {
            mDrawPaint!!.strokeWidth=mDrawPath!!.brushThickness
            mDrawPaint!!.color=mDrawPath!!.color
@@ -70,15 +79,26 @@ class DrawingView(context:Context,attrs: AttributeSet): View(context,attrs) {
 
             }
             MotionEvent.ACTION_UP->{
+                // adding each path when its done
+                mPaths.add(mDrawPath!!)
                 mDrawPath=CustomPath(color,mBrushSize)
+
+
             }
             else->return false
 
         }
+        //is called to force a redraw of the view, which triggers the onDraw() method to update the canvas with the new drawing.
         invalidate()
 
         return true
 
+    }
+
+    fun setSizeForBrush(newSize:Float){
+        mBrushSize=TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize,resources.displayMetrics)
+        mDrawPaint!!.strokeWidth=mBrushSize
     }
     internal inner class CustomPath(var color:Int,
                                     var brushThickness:Float): Path() {
--- app/src/main/java/ud/demo/drawingapp/MainActivity.kt
+++ app/src/main/java/ud/demo/drawingapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package ud.demo.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private var drawingView:DrawingView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView=findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        val brush :ImageButton = findViewById(R.id.brush)
+        brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+
+
+    }
+    private fun showBrushSizeChooserDialog(){
+        val brushDailog = Dialog(this)
+        brushDailog.setContentView(R.layout.dialog_brush_size)
+        brushDailog.setTitle("Brush size: ")
+        val smallBtn = findViewById<ImageButton>(R.id.small_brush)
+        smallBtn.setOnClickListener{
+            drawingView!!.setSizeForBrush(10.toFloat())
+            brushDailog.dismiss()
+        }
+
+        val mediumBtn = findViewById<ImageButton>(R.id.medium_brush)
+        smallBtn.setOnClickListener{
+            drawingView!!.setSizeForBrush(20.toFloat())
+            brushDailog.dismiss()
+        }
+
+        val largeBtn = findViewById<ImageButton>(R.id.large_brush)
+        smallBtn.setOnClickListener{
+            drawingView!!.setSizeForBrush(30.toFloat())
+            brushDailog.dismiss()
+        }
+
+
+        brushDailog.show()
+
     }
 }
